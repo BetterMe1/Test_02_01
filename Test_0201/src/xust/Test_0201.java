37f7d95c742e0fa220245c8182ac7b88686eeb0f
BetterMe1 <1754217281@qq.com>
2019-02-02 12:09:18 +0800

    提交

--- Test_0201/src/xust/Test_0201.java
+++ Test_0201/src/xust/Test_0201.java
@@ -18,16 +18,24 @@ public class Test_0201 {
 	public static void main(String[] args) {
 		Solution So = new Solution();
 		int[] nums = {1,2,3};
-		System.out.println(So.minMoves(nums));
+		System.out.println(So.minMoves2(nums));
 	}
 }
+/*
+ * 分析：
+ * 正向思考：计算数组最大元素与最小元素的差，给小于最大元素的数组元素都加上这个数，判断数组的元素是否已全部相等，如果不相等，接着执行上述操作，直到全部相等，
+ * 为了得到最大元素和最小元素，可以使用数组排序。
+ * 逆向思考：每次移动让除去最大元素的剩余n-1个数加1，相当于每次移动让选定的数减1，所以最少移动次数其实就是所有元素减去最小元素的和。
+ */
 class Solution {
-    public int minMoves(int[] nums) {
+    public int minMoves1(int[] nums) {
     	int count = 0;
     	while(compare(nums) == false){
     		Arrays.sort(nums);//数组排序
     		int n =nums[nums.length-1]-nums[0];
-    		nums[nums.length-1] -= n;
+    		for(int i = 0; i<nums.length-1; i++){
+    			nums[i] += n;
+    		}
             count += n;
     	}
     	return count;
@@ -40,5 +48,15 @@ class Solution {
     		}
     	}
     	return true;
+    }
+    public int minMoves2(int[] nums) {
+    	int sum = nums[0];
+    	int min = nums[0];
+    	for(int i=1; i<nums.length; i++){
+    		sum += nums[i];
+    		if(nums[i] <min)
+    			min = nums[i];
+    	}
+    	return sum -nums.length*min; 
     }
 }
